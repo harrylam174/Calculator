bb1d1d8d8077b2a71bdb1df5bece7a839c0ee601
Harry Lam <harry.lam174@gmail.com>
2024-12-01 17:13:00 -0500

    Add test.cpp, modify CMakeList to use test.cpp instead of main.cpp, modify MakefileTemplate.txt to ensure correctness

--- main.cpp
+++ main.cpp
@@ -1,22 +1,11 @@
 #include "calculator.h"
 #include <iostream>
-#include <gtest/gtest.h>
-
-TEST(CALCULATOR, ADD) {
-    ASSERT_EQ(Calculator::add(2, 2), 4);
-    ASSERT_EQ(Calculator::add(-2, 3), 1);
-    ASSERT_EQ(Calculator::add(1, -5), -4);
-}
-
-TEST(CALCULATOR, SUBTRACT) {
-    ASSERT_EQ(Calculator::subtract(2, 2), 0);
-    ASSERT_EQ(Calculator::subtract(-2, 3), -5);
-    ASSERT_EQ(Calculator::subtract(1, -5), 6);
-}
 
 int main(int argc, const char * argv[]) {
-    testing::InitGoogleTest();
-    return RUN_ALL_TESTS();
+    double num1 = Calculator::add(2, 2);
+    double num2 = Calculator::power(3, 2);
+    double result = Calculator::factorial(num2 - num1);
+    cout << result << endl;
 }
 
 
---
+++ test.cpp
@@ -0,0 +1,21 @@
+#include "calculator.h"
+#include <iostream>
+#include <gtest/gtest.h>
+
+TEST(CALCULATOR, ADD) {
+    ASSERT_EQ(Calculator::add(2, 2), 4);
+    ASSERT_EQ(Calculator::add(-2, 3), 1);
+    ASSERT_EQ(Calculator::add(1, -5), -4);
+}
+
+TEST(CALCULATOR, SUBTRACT) {
+    ASSERT_EQ(Calculator::subtract(2, 2), 0);
+    ASSERT_EQ(Calculator::subtract(-2, 3), -5);
+    ASSERT_EQ(Calculator::subtract(1, -5), 6);
+}
+
+int main(int argc, const char * argv[]) {
+    testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
+}
+
